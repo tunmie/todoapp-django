bf10f68ed6379ce075c60ffbfc34afa451c3fc30
tunmie <victoroluwasomidotun@gmail.com>
2022-09-05 04:48:45 +0100

    todolist api route implemented

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import TodoSerializer
+from todo.models import Todo
 
 # Create your views here.
+class TodoList(generics.ListAPIView):
+    serializer_class = TodoSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user).order_by('-created')
