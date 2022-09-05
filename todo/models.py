63c9fa1831f84ae6692ec0c84b4f4a7f6a928fd5
tunmie <victoroluwasomidotun@gmail.com>
2022-09-05 04:31:58 +0100

    getting to serialize

---
+++ todo/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    memo = models.TextField(blank=True)
+
+    # set to current time
+    created = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+
+    # user who posted this
+    users = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
